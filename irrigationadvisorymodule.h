ad6cac2d043955c678ab8eb4fc44218bcd80542a
PIYUSH DWIVEDI <dwivedipiyush145@gmail.com>
2026-02-09 17:35:28 +0530

    Delete irrigationadvisorymodule.h

--- irrigationadvisorymodule.h
+++
@@ -1,9 +0,0 @@
-#ifndef IRRIGATIONADVISORYMODULE_H
-#define IRRIGATIONADVISORYMODULE_H
-
-#include<string>
-
-std::string irrigationadvisory(std::string cropsuggested,std::string season,float rainfall,std::string climatewarning,std::string soiltype);
-
-
-#endif
