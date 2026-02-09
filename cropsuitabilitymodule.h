6de8b9a9af3855901361d7f75d895034da4a42a9
PIYUSH DWIVEDI <dwivedipiyush145@gmail.com>
2026-02-09 17:34:21 +0530

    Delete cropsuitabilitymodule.h

--- cropsuitabilitymodule.h
+++
@@ -1,8 +0,0 @@
-#ifndef CROPSUITABILITYMODULE_H
-#define CROPSUITABILITYMOUDLE_H
-
-#include<string>
-
-std::string cropsuitability(std::string& soiltype,std::string& season,std::string& farmerchoice,float temperature,float rainfall);
-
-#endif
