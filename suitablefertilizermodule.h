164d21f455f7fd5d5b6b608d42d7d370682d84b0
PIYUSH DWIVEDI <dwivedipiyush145@gmail.com>
2026-02-09 17:31:53 +0530

    Delete suitablefertilizermodule.h

--- suitablefertilizermodule.h
+++
@@ -1,8 +0,0 @@
-#ifndef SUITABLEFERTILIZERMODULE_H
-#define SUITABLEFERTILIZERMODULE_H
-
-#include<string>
-
-std::string suitablefertilizer(std::string crop,std::string soiltype,std::string season,std::string climatewarning);
-
-#endif
