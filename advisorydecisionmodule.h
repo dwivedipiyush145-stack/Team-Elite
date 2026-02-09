da34c71c50852a7ccc9bf8e1412b90f244059f0f
PIYUSH DWIVEDI <dwivedipiyush145@gmail.com>
2026-02-09 17:33:36 +0530

    Delete advisorydecisionmodule.h

--- advisorydecisionmodule.h
+++
@@ -1,9 +0,0 @@
-#ifndef ADVISORYDECISIONMODULE_H
-#define ADVISORYDECISIONMODULE_H
-
-#include<string>
-
-std::string advisorydecision(std::string climatewarning,std::string cropsuggested,std::string irrigationadvice);
-
-
-#endif
